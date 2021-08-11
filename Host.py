8c3ad7ea683dbc15a147104368ab3843955de719
Balavenkatgopinath <79308182+Balavenkatgopinath@users.noreply.github.com>
2021-08-11 20:00:46 +0100

    Add files via upload

---
+++ Host.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install sockets')
+
+
+# In[ ]:
+
+
+import socket
+import sys
+import time
+
+s=socket.socket()
+host=socket.gethostname()
+print("server will start on host:",host)
+port = 1234
+s.bind((host,port))
+print("Server is bound successfully")
+s.listen(1)
+conn,addr=s.accept()
+print(addr,"has connected")
+while 1:
+    message=input(str("You:>>"))
+    message=message.encode()
+    conn.send(message)
+    incoming_message=conn.recv(1024)
+    incoming_message=incoming_message.decode()
+    print("Client:>>",incoming_message)
+                      
+
+
+# In[ ]:
+
+
+
+
