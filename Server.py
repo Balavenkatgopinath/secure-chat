3970b7c202d9c5f3fbc22773e2f30c363bb98a75
Balavenkatgopinath <79308182+Balavenkatgopinath@users.noreply.github.com>
2021-08-11 20:01:27 +0100

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+import sys
+import time
+
+s=socket.socket()
+host=input(str("Please enter host name:"))
+port=1234
+try:
+    s.connect((host,port))
+    print("Connected to server")
+except:
+    print("Connection to server is failed:")
+while 1:
+    incoming_message=s.recv(1024)
+    incoming_message=incoming_message.decode()
+    print("Server:>>",incoming_message)
+    message=input(str("You:>>"))
+    message=message.encode()
+    s.send(message)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
